1c14fbe69d0c8a734a608a708a6c9444b61d821b
avyniee <avyagard@gmail.com>
2024-09-15 18:01:38 -0300

    Implementando o MongoDB e aprimorando o código

--- src/app.js
+++ src/app.js
@@ -1,54 +1,18 @@
-import express from "express"
+import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("Erro ao conectar no MongoDB", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conectado no MongoDB!!")
+});
 
 const app = express()
-app.use(express.json())
-
-const livros = [
-    {
-        id: 1,
-        nome: "O Senhor dos Anéis",
-    },
-    { 
-        id: 2,
-        nome: "Sherlock Holmes",
-    }
-
-]
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    })
-}
-
-app.get("/", (req, res) => {
-    res.status(200).send("Curso de Node.js");
-});
-
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
-});
-
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id)
-    res.status(200).json(livros[index])
-});
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso!!")
-});
-
-app.put("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].nome = req.body.nome;
-    res.status(200).json(livros[index]);
-});
-
-app.delete("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(200).send("Livro removido com sucesso!!");
-})
+routes(app);
 
 export default app;
